739d0b5fedefc6221275e5a1d897943922bc844e
hode69 <43639379+hode69@users.noreply.github.com>
2024-10-12 00:24:07 +0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+function toggle1(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup1 = document.getElementById('popup1');
+    popup1.classList.toggle('active');
+}
+
+function toggle2(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup2 = document.getElementById('popup2');
+    popup2.classList.toggle('active');
+}
